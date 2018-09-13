054852a342e40d60b1c3a2ae7bd3b2ecab587c07
Shiming Yang <shimingyoung@gmail.com>
2018-09-13 16:13:00 -0400

    projection

--- algorithms.py
+++ algorithms.py
@@ -4,84 +4,129 @@ Created on Mon Jul  2 15:43:23 2018
 
 @author: Admin
 """
+from projections import project_L1_ball
+from projections import project_L1_infinity_ball
+from norms import norm_1_infinity
+from norms import norm_11
+import matplotlib.pyplot as plt
+import numpy as np
 
-def fista_function(X, y):
-    loss_old = 0------------------------------------
+def sigmoid(Y2):
+    return(1/(1+np.exp(-Y2)))
+
+def logistic_loss_function(X1, Y1, S1, B1, lambda_1, lambda_2, n_var, n_task):
+    N = len(X1)
+    Y2 = np.matmul(X1,(B1+S1))
+    Y2 = sigmoid(Y2)
+    class_cost_1 = np.multiply(-Y1, np.log(Y2))
+    class_cost_2 = np.multiply((1 -Y1), np.log(1 - Y2))
+    total = class_cost_1 - class_cost_2
+    total_loss = total.sum()/N
+    final_loss = total_loss + lambda_1*norm_11(S1.reshape((n_var, n_task))) + lambda_2*norm_1_infinity(B1.reshape((n_var, n_task)))
+    a1 = np.subtract(Y2, Y1)
+    gradient = np.matmul(X1.T,a1)/N
+    return(final_loss, total_loss, gradient)
+
+def first_condition(proj_S, proj_B, X1, Y1, lambda_1, lambda_2, loss_option, n_var, n_task):
+    s1 = proj_S.ravel().reshape(-1,1)
+    b1 = proj_B.ravel().reshape(-1,1)
+    if loss_option == "logistic":
+        loss, log_loss, gradient_updated = logistic_loss_function(X1, Y1, s1, b1, lambda_1, lambda_2, n_var, n_task )
+        return(loss, log_loss, gradient_updated)
+    else:
+        activation = np.matmul(X1,(s1+b1))
+        log_loss_2 = (np.linalg.norm(Y1 - activation))**2
+        loss =  log_loss_2 + lambda_1*norm_11(proj_S) + lambda_2*norm_1_infinity(proj_B)
+        a1 = np.subtract(activation, Y1)
+        gradient_updated = 2*np.matmul(X1.T,a1)
+        gradient_updated = gradient_updated.reshape(proj_S.shape[0], proj_S.shape[1])
+        return(loss,log_loss_2, gradient_updated)
+
+def second_order_expansion(loss_old, proj_S, proj_B, S, B, gradient_old, L, lambda_1, lambda_2):
+    first_term = loss_old
+    S = S.reshape(proj_S.shape[0], proj_S.shape[1])
+    B = B.reshape(proj_B.shape[0], proj_B.shape[1])
+    x_k = (proj_S+proj_B - (S+B))
+    second_term = np.inner((x_k).ravel(), gradient_old.ravel())    
+    third_term = L/2*((np.linalg.norm((x_k), ord=2))**2)
+    fourth_term = lambda_1*norm_11(proj_S)+ lambda_2*norm_1_infinity(proj_B)
+    output = first_term+second_term+third_term+fourth_term
+    return(output)
+
+def FISTA_algorithm_multitasking(X1, Y1,  n_var, n_task, max_iteration, loss_option=None):
+    n_obs= X1.shape[0]
+#    n_task = 139
+#    n_var = 28
+    plt.figure(figsize=(1,1))
     lambda_1 = 1
-    lambda_2 = 9
-    L_0 = 15
-    eta = 10
-    random_number = 30
-    S = np.random.normal(0, 1,(X_train.shape[1], y_train.shape[1]))
-    B = np.random.normal(0,1, (X_train.shape[1], y_train.shape[1]))
-    print(S.shape)
-    print(B.shape)
+    lambda_2 = 1
+    eta = 3
+    S1 = np.random.normal(0,1, (n_var, n_task))
+    B1 = np.random.normal(0,1, (n_var, n_task))
+    S1 = S1.flatten()
+    B1 = B1.flatten()
+    S_old = S1
+    B_old = B1
+    final_loss_list = []
+    Y2 = np.zeros((Y1.shape[0], Y1.shape[1]))
+#    max_iteration = 2000
+    loss_old = 0
+    rand_iteration = 50
+    L_new = 10
+    precision = 1e-3
+    t_1 = 1
     total_loss_list = []
-    activation = np.zeros((y_train.shape[0], y_train.shape[1]))
-    activation_1 = np.zeros((y_train.shape[0], y_train.shape[1]))
-    max_iteration = 100
-    precision = 1e-1
-    p =0
-    for j in range(max_iteration):
-        for p in range(0,len(X_train)):
-            hypothesis = np.dot(np.transpose(S+B), X_train[p])
-            activation[p]= sigmoid_activation(hypothesis)
-        loss_1 = log_loss(y_train, activation)
-        print("This is first regularizer", norm_11(S))
-        print("This is second regularizer", norm_1_infinity(B))
-        total_loss = loss_1 + lambda_1*norm_11(S)+ lambda_2*norm_1_infinity(B)
-        total_loss_list.append(total_loss)
-        print(total_loss)
-        if abs(loss_old - total_loss) == precision:
-            return(S, B, total_loss_list, p, activation)
-        gradient = gradient_logistic_function(activation, X_train, y_train)
-        for i in range(random_number):
-            if i == 0:
-                L_new = (eta**i)*(L_0)
-                print(L_new)
-                t = 1
-            else:
-                L_new = (eta**i)*L_old
-                print(L_new)
-                t = 1/L_new
-            proj_S = project_L1_ball((S - t*gradient).T, lambda_1)
-            proj_B = projected_L1_infinity_ball((B - t*gradient).T, lambda_2)
-            proj_S = proj_S.T
-            proj_B = proj_B.T
-            loss_old,log_loss_2,gradient_old = first_condition(proj_S, proj_B, X_train,y_train, lambda_1, lambda_2)
-            b = second_order_expansion(log_loss_2, proj_S, proj_B, S, B, gradient_old, L_new, lambda_1, lambda_2)
-            print("This is loss_old", loss_old)
-            print("This is second_part", b)
+    norm1_list = []
+    norminf_list = []
+    for i in range(max_iteration):
+        if loss_option == "logistic":
+            final_loss, total_loss, gradient = logistic_loss_function(X1, Y1, S1, B1, lambda_1, lambda_2, n_var, n_task)
+        else:
+            Y2 = np.matmul(X1, (B1+S1))
+            Y2 = Y2.reshape(-1,1)
+            total_loss = np.linalg.norm(Y2- Y1)**2
+            total_loss_list.append(total_loss)
+            final_loss = total_loss + lambda_1*norm_11(S1.reshape((n_var, n_task))) + lambda_2*norm_1_infinity(B1.reshape((n_var, n_task)))
+            print(final_loss)
+            final_loss_list.append(final_loss)
+            a1 = np.subtract(Y2, Y1)
+            gradient = 2*np.matmul(X1.T,a1)
+        if abs(loss_old - final_loss) <= precision:
+            return(S1, B1)
+        norm1_list.append(norm_11(S1.reshape((n_var, n_task))))
+        norminf_list.append(norm_1_infinity(B1.reshape((n_var, n_task))))
+        """
+        Now trying to find best possible Lipchitz constant for the given function
+        """
+        for j in range(rand_iteration):
+            L_new = (eta**j)*(L_new)
+            proj_S = project_L1_ball((S1.reshape((n_var, n_task)) - gradient.reshape((n_var, n_task))/L_new), lambda_1/L_new)        
+            proj_B = project_L1_infinity_ball((B1.reshape((n_var, n_task)) - gradient.reshape((n_var, n_task))/L_new), lambda_2/L_new)
+            """ checking the condition"""
+            loss_old,log_loss_2,gradient_old = first_condition(proj_S, proj_B, X1 ,Y1, lambda_1, lambda_2, loss_option, n_var, n_task)
+            b = second_order_expansion(total_loss, proj_S, proj_B, S1, B1, gradient, L_new, lambda_1, lambda_2)
             if loss_old <= b:
-                S = proj_S - S
-                B = proj_B - B
                 break
             else:
-                L_old = L_new
-                S = proj_S - S
-                B = proj_B - B             
-#                if L_new == L_old:
-#                    break
-        print("This is the new L", L_new)
-        t_1 = 1/L_new
-        t_new = (1 + np.sqrt(1 + 4 *(t_1**2)))/2
+                print("Condition not true")
+        """Now since we have found the Lipchitz constant we update the parameters"""
+        proj_S = project_L1_ball((S1.reshape((n_var, n_task)) - gradient.reshape((n_var, n_task))/L_new), lambda_1/L_new)
+        proj_B = project_L1_infinity_ball((B1.reshape((n_var, n_task)) - gradient.reshape((n_var, n_task))/L_new), lambda_2/L_new)
+        S1 = proj_S.ravel()
+        B1 = proj_B.ravel()
+        t_new = (1 + np.sqrt(1 + 4 *(t_1**2))/2)
         eta_1= (t_1-1)/t_new
-        if j == 0:
-            S_old = 0
-            B_old = 0 
-        else:
-            S = S + eta_1*(S - S_old)
-            B = B + eta_1*(B - B_old) 
-        S_old = S
-        B_old = B
-        loss_old = total_loss
-    return(S, B, total_loss_list, p, activation)
-S, B, total_loss_list, p, activation_1 = fista_function(X_train, y_train)
-print(p)
-plt.plot(np.array(total_loss_list))
-plt.show()
-
-
+        S1 = S1 + eta_1*(S1 - S_old)    
+        B1 = B1 + eta_1*(B1 - B_old)
+#        diff = np.linalg.norm(S1)
+        S_old = S1
+        B_old = B1
+        loss_old = final_loss
+        t_1 = t_new
+#    plt.matshow(S1.reshape((n_var, n_task)))
+#    plt.matshow(B1.reshape((n_var, n_task)))
+    return(S1, B1)
+#%%
 def ADMM_with_least_square(X_train, y_train):
     loss_old = 0
     lambda_1 = 0.009
@@ -98,33 +143,110 @@ def ADMM_with_least_square(X_train, y_train):
     theta_1 = S+B
     Z_k = S+B
     N = len(X_train)
-    
-#    bias = np.random.normal(0,1,(X_train.shape[0],1))
-#    bias = np.ones((X_train.shape[0],1))
-#    for j in range(max_iteration):
-#        loss_1 = 0
-#        for p in range(len(X_train)):
-#            hypothesis = np.dot(np.transpose(theta_1), X_train[p])
-#            activation[p] = hypothesis
-#        loss_1 = np.linalg.norm(np.subtract(y_train, activation))**2
-#        total_loss = ((1/2) *loss_1 + lambda_1*(norm_11(S))+ lambda_2*norm_1_infinity(B))
-#        print(total_loss)
-#        total_loss_list.append(total_loss)
-#        if abs(total_loss - loss_old) <= precision:
-#            return(theta_1, S, B, total_loss_list, activation)
-#        theta_1 = solvin_argmin_least_square(activation, X_train, y_train, theta_1, Z_k, U_k, rho)
-#        proj_S = project_L1_ball((theta_1 -B + U_k), lambda_1/rho)
-#        proj_B = projected_L1_infinity_ball((theta_1 -  S + U_k), lambda_2/rho)
-#        Z_k = proj_S+ proj_B
-#        U_k = U_k + theta_1 - Z_k
-#        S = proj_S 
-#        B = proj_B
-#        loss_old = total_loss
-#    print(j)
-#    return(theta_1, S, B, total_loss_list, activation)
-theta_1, S, B, total_loss_list, activation = ADMM_with_least_square(X_train, y_train)
-plt.plot(np.array(total_loss_list))
-plt.show()
+    bias = np.random.normal(0,1,(X_train.shape[0],1))
+    bias = np.ones((X_train.shape[0],1))
+    for j in range(max_iteration):
+        loss_1 = 0
+        for p in range(len(X_train)):
+            hypothesis = np.dot(np.transpose(theta_1), X_train[p])
+            activation[p] = hypothesis
+        loss_1 = np.linalg.norm(np.subtract(y_train, activation))**2
+        total_loss = ((1/2) *loss_1 + lambda_1*(norm_11(S))+ lambda_2*norm_1_infinity(B))
+        print(total_loss)
+        total_loss_list.append(total_loss)
+        if abs(total_loss - loss_old) <= precision:
+            return(theta_1, S, B, total_loss_list, activation)
+        theta_1 = solvin_argmin_least_square(activation, X_train, y_train, theta_1, Z_k, U_k, rho)
+        proj_S = project_L1_ball((theta_1 -B + U_k), lambda_1/rho)
+        proj_B = projected_L1_infinity_ball((theta_1 -  S + U_k), lambda_2/rho)
+        Z_k = proj_S+ proj_B
+        U_k = U_k + theta_1 - Z_k
+        S = proj_S 
+        B = proj_B
+        loss_old = total_loss
+    print(j)
+    return(theta_1, S, B, total_loss_list, activation)
+#theta_1, S, B, total_loss_list, activation = ADMM_with_least_square(X_train, y_train)
+#plt.plot(np.array(total_loss_list))
+#plt.show()
 #%%
-w1 = roc_auc_score_1(y_train, activation_1)
-print(w1) 
+
+#def fista_function(X, y):
+#    loss_old = 0------------------------------------
+#    lambda_1 = 1
+#    lambda_2 = 9
+#    L_0 = 15
+#    eta = 10
+#    random_number = 30
+#    S = np.random.normal(0, 1,(X_train.shape[1], y_train.shape[1]))
+#    B = np.random.normal(0,1, (X_train.shape[1], y_train.shape[1]))
+#    print(S.shape)
+#    print(B.shape)
+#    total_loss_list = []
+#    activation = np.zeros((y_train.shape[0], y_train.shape[1]))
+#    activation_1 = np.zeros((y_train.shape[0], y_train.shape[1]))
+#    max_iteration = 100
+#    precision = 1e-1
+#    p =0
+#    for j in range(max_iteration):
+#        for p in range(0,len(X_train)):
+#            hypothesis = np.dot(np.transpose(S+B), X_train[p])
+#            activation[p]= sigmoid_activation(hypothesis)
+#        loss_1 = log_loss(y_train, activation)
+#        print("This is first regularizer", norm_11(S))
+#        print("This is second regularizer", norm_1_infinity(B))
+#        total_loss = loss_1 + lambda_1*norm_11(S)+ lambda_2*norm_1_infinity(B)
+#        total_loss_list.append(total_loss)
+#        print(total_loss)
+#        if abs(loss_old - total_loss) == precision:
+#            return(S, B, total_loss_list, p, activation)
+#        gradient = gradient_logistic_function(activation, X_train, y_train)
+#        for i in range(random_number):
+#            if i == 0:
+#                L_new = (eta**i)*(L_0)
+#                print(L_new)
+#                t = 1
+#            else:
+#                L_new = (eta**i)*L_old
+#                print(L_new)
+#                t = 1/L_new
+#            proj_S = project_L1_ball((S - t*gradient).T, lambda_1)
+#            proj_B = projected_L1_infinity_ball((B - t*gradient).T, lambda_2)
+#            proj_S = proj_S.T
+#            proj_B = proj_B.T
+#            loss_old,log_loss_2,gradient_old = first_condition(proj_S, proj_B, X_train,y_train, lambda_1, lambda_2)
+#            b = second_order_expansion(log_loss_2, proj_S, proj_B, S, B, gradient_old, L_new, lambda_1, lambda_2)
+#            print("This is loss_old", loss_old)
+#            print("This is second_part", b)
+#            if loss_old <= b:
+#                S = proj_S - S
+#                B = proj_B - B
+#                break
+#            else:
+#                L_old = L_new
+#                S = proj_S - S
+#                B = proj_B - B             
+##                if L_new == L_old:
+##                    break
+#        print("This is the new L", L_new)
+#        t_1 = 1/L_new
+#        t_new = (1 + np.sqrt(1 + 4 *(t_1**2)))/2
+#        eta_1= (t_1-1)/t_new
+#        if j == 0:
+#            S_old = 0
+#            B_old = 0 
+#        else:
+#            S = S + eta_1*(S - S_old)
+#            B = B + eta_1*(B - B_old) 
+#        S_old = S
+#        B_old = B
+#        loss_old = total_loss
+#    return(S, B, total_loss_list, p, activation)
+#S, B, total_loss_list, p, activation_1 = fista_function(X_train, y_train)
+#print(p)
+#plt.plot(np.array(total_loss_list))
+#plt.show()
+
+
+
+#%%
--- projections.py
+++ projections.py
@@ -2,143 +2,111 @@
 """
 Created on Mon Jul  2 15:37:32 2018
 
-@author: Admin
+@author: S. Yang
 """
 import numpy as np
-from norms import *
+#from norms import norm_1_infinity
+import copy
 
+#from norms import norm_11
 # a collection of projection operators
-def project_L1_ball(D, tau):
+def project_L11(D, tau):
     # min (1/2) ||X-C||_2^2, s.t. ||X||_1 <= t
-    if norm_11(D) <= tau:
-        return(D)
+    X = np.multiply(np.sign(D), np.clip(np.absolute(D)-tau, 0, None) )
+    return X
+
+def project_L1inf(A, C):
+    # follow the c code 'projL1Inf.c' from https://github.com/doksa/altginv/blob/master/projL1Inf.c
+    maxes = np.max(np.absolute(A), axis = 1)
+    normOfA = np.sum(maxes)
+    if normOfA <= C:
+        return A
     else:
-        q = np.sign(D)
-    #    print(q)
-        a = np.zeros((D.shape[0], D.shape[1]))
-        for i in range(len(D)):
-            for j in range(D.shape[1]):
-                a[i][j] = np.maximum(0, (abs(D[i][j])-tau))
-        return np.multiply(a,q)    
+        #indices where maxes > 0
+        nRows, nCols = A.shape
+        sparseMap = np.where(maxes > 0)
+        n_nonzeros = len(sparseMap[0]) # number of rows that row abs sum >0
 
-def project_L1_infinity_ball(A, C):
-    C = C*norm_1_infinity(A)
-    if norm_1_infinity(A) <= C:
-        return(A)
-    else:
-        Q = float(norm_1_infinity(A))
-        W = np.absolute(A)
-        #print(W)
-        C1 = 0.001*Q
-        B = np.zeros((A.shape[0], A.shape[1]))
-        S = np.zeros((W.shape[0], W.shape[1]))
-        for i in range(W.shape[0]):
-            S[i] = np.sort(W[i], kind="mergesort")
-            S[i] = S[i][::-1]
-        y = np.array(np.repeat([0], S.shape[0]),)
-        y = y.reshape(1, -1)
-        S = np.append(S, y.T, axis=1)
-        S1 = S.ravel()
-        r = np.zeros((S.shape[0], S.shape[1]))
-        row_index_list = []
-        for j in range(S.shape[0]):
-            for k in range(S.shape[1]):
-                r[j][k] = (np.sum(S[j][:k] - S[j][k]))
-                row_index_list.append(j)
-        #        print(r[j][k])
-        r1 = r.ravel()
-        r2_array = np.argsort(r1)
-        r1 = r1[r2_array]
-        ks = np.zeros((len(W),))
-        idx = 0
-        while r1[idx] <= 0:
-            a1 = row_index_list[r2_array[idx]]
-            ks[a1] = ks[a1]+1
-            idx = idx+1
-        gradient = 0
-        for i in range(len(ks)):
-            gradient += -1/ks[i]
-        norm = Q
-        idx = idx-1
-        next_norm = gradient*(r1[idx+1] - r1[idx]) + norm
-        while (next_norm > C1):
-            idx = idx+1
-            norm = next_norm
-            r_1 = row_index_list[r2_array[idx]]
-            if True:
-                gradient+=(-1/ks[r_1])
-                ks[r_1] = ks[r_1] +1
-                if ks[r_1] <= W.shape[1]:
-                    gradient += 1/ks[r_1]
+        S = np.zeros([n_nonzeros, nCols+1])
+        S[:,0:nCols] = np.absolute(A)
+        
+        sortedS = -np.sort(-S, axis = 1, kind='quicksort')
+        res = np.cumsum(sortedS, axis = 1)
+        J = np.tile(range(1,(nCols+1)), (n_nonzeros, 1) )
+        Rvalue = np.hstack((np.zeros([n_nonzeros,1]), res[:,0:nCols] - np.multiply(sortedS[:,1:(nCols+1)], J) ) )
+        RrowInd = np.transpose(np.tile(range(0,n_nonzeros), (nCols+1,1)))
+        R = Rvalue.ravel(order='F')
+        Rind = RrowInd.ravel(order='F')
+        
+        locs = np.argsort(R, kind='quicksort')
+        R = R[locs]
+        Rind = Rind[locs]
+
+        idxR = 0
+        Ks = np.zeros([n_nonzeros,1])
+        
+        while (R[idxR] <= 0.0):
+            Ks[Rind[idxR]] = Ks[Rind[idxR]] + 1
+            idxR = idxR + 1
+        idxR = idxR - 1
+        
+        Gradient = sum( -1.0 / Ks)
+        norm = normOfA
+        nextNorm = Gradient * (R[idxR+1] - R[idxR]) + norm
+        
+        while (nextNorm > C):
+            idxR = idxR + 1
+            norm = nextNorm
+            r1 = Rind[idxR]
+            Gradient = Gradient + 1.0 / Ks[r1]
+            Ks[r1] = Ks[r1] + 1
+            if (Ks[r1] <= nCols):
+                Gradient = Gradient - 1.0/Ks[r1]
+            nextNorm = Gradient * (R[idxR+1] - R[idxR]) + norm
+            
+        theta = (C - norm + Gradient * R[idxR]) / Gradient
+        
+        #sum1 = 0
+        mu = np.zeros([n_nonzeros, 1])
+        for i in range(n_nonzeros):
+            if Ks[i] <= nCols:
+                j = idxR
+                while Rind[j] != i:
+                    j = j - 1
+                mu[i] = sortedS[i][int(Ks[i]-1.0)] - (theta - R[j]) / Ks[i]
             else:
-                ks[r_1] = ks[r_1]+1
-                gradient  = 0
-                for p in range(len(ks)):
-                    if ks[p] <= W.shape[1]:
-                        gradient += -1/ks[p]
-            next_norm = gradient *(r1[idx+1] - r1[idx]) + norm
-        theta = (C - norm + gradient*r1[idx])/gradient
-        #calculating mu
-        U = np.zeros((len(W), ))
-        sum_1 = 0
-        for x in range(S.shape[0]):
-            if ks[x] <= A.shape[1]:
-                y = idx
-                while (row_index_list[r2_array[y]] != x):
-                    y = y-1
-                    U[x] = S1[int(x*(S.shape[1])+ks[x]-1)] - (1/ks[x])*(theta - r1[y])
-            else:
-                U[x] = 0    
-            sum_1+= U[x]
-        for c in range(A.shape[0]):
-            for d in range(A.shape[1]):
-                if A[c][d] >= U[c]:
-                    B[c][d] = U[c]
-                elif A[c][d] <= U[c]:
-                    B[c][d] = A[c][d]
-                else:
-                    B[c][d] = 0
-        return(np.sign(A)*B)    
+                mu[i] = 0.0
+            #sum1 = sum1 + mu[i]
+        
+        B = copy.deepcopy(A)
+        for i in range(n_nonzeros):
+            flag = np.logical_or(B[i,:] > mu[i], B[i,:] < -mu[i])
+            loc1 = np.where(np.logical_and(flag, A[i,:] >= 0.0))
+            B[i, loc1] = mu[i]
+            loc1 = np.where(np.logical_and(flag, A[i,:] < 0.0))
+            B[i, loc1] = -mu[i]
+    return B
 
 
-def project_L1inf(D, tau):
-    B = np.copy(D) # make a hard copy of D
+def project_L21(D, tau):
+    n_var, n_task = D.shape
+    c = np.clip(1 - tau / np.sqrt(np.sum(np.square(D), axis=1) ), 0, None)
+    X = np.multiply(np.tile(c, [1, n_task]), D)
+    return X
     
-    l1infnorm_D = norm_1_infinity(D)
-    if l1infnorm_D <= tau:
-        return B
+def project_trace(D, tau):
+    n_var, n_task = D.shape
+    if (n_var > n_task):
+        U, S, V = np.linalg.svd(D, full_matrices=True)
     else:
-        nRow, nCol = np.shape(D)
-        rowAbsSum = np.sum(np.fabs(D), axis = 1)
-        nonZeroRows = np.where(rowAbsSum > 0)
-        
-        S = np.zeros(nonZeroRows, nCol+1)
-        R = np.zeros(nonZeroRows, nCol+1)
-        R_idx = np.zeros(nonZeroRows, nCol+1)
-        
-        for i in nonZeroRows:
-            S[i, 0:-1] = np.sort(np.fabs(A[i,]))[::-1]
-        
-        for i in nonZeroRows:
-            for j in nCols:
-                R_idx[i,j]  = i
-                R[i, j+1] =  
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        U, S, V = np.linalg.svd(np.transpose(D), full_matrices=True)
+    thres = np.diag(S) - tau / 2.0
+    diag_S = np.clip(np.diag(S) - tau / 2.0, 0.0, None)
+    L_hat = U * np.diag(thres) * V
+    L_tn = sum(diag_S)
+    return L_hat, L_tn
+
+def project_box(v, l, u):
+    #Project a point onto a box (hyper-rectangle)
+    x = np.clip(v, l, u)
+    return x
